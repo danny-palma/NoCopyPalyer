2780bd7c42860304d0514b2a4232eb39d0848589
Daniel Palma <danielpalma2003@hotmail.com>
2021-04-16 14:31:51 -0500

    fisrt commit

---
+++ Window_main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoCopyPlayer
+{
+    public partial class Window_main : Form
+    {
+        public Window_main()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
